b8ecb4d4a831d2f35459186928a18207398fce8e
JBGruber <j.gruber.1@research.gla.ac.uk>
2021-08-19 21:17:34 +0200

    Added dailymail.co.uk scraper (#1)

---
+++ R/deliver_dailymail_co_uk.R
@@ -0,0 +1,54 @@
+
+pb_deliver_paper.www_dailymail_co_uk <- function(x, verbose = NULL, ...) {
+
+  . <- NULL
+
+  if (is.null(verbose)) verbose <- getOption("paperboy_verbose")
+
+  if (!"tbl_df" %in% class(x))
+    stop("Wrong object passed to internal deliver function: ", class(x))
+
+  if (verbose) message("\t...", nrow(x), " articles from huffpost")
+
+  pb <- make_pb(x)
+
+  purrr::map_df(x$content_raw, function(cont) {
+
+    if (verbose) pb$tick()
+
+    html <- rvest::read_html(cont)
+
+    # datetime
+    datetime <- html %>%
+      rvest::html_elements("[property=\"article:published_time\"]") %>%
+      rvest::html_attr("content") %>%
+      lubridate::as_datetime()
+
+    # headline
+    headline <- html %>%
+      rvest::html_elements("[property =\"mol:headline\"]") %>%
+      rvest::html_attr("content")
+
+    # author
+    author <- html %>%
+      rvest::html_elements("[name =\"author\"]") %>%
+      rvest::html_attr("content")
+
+    # text
+    text <- html %>%
+      rvest::html_elements("[itemprop=\"articleBody\"]") %>%
+      rvest::html_elements("p") %>%
+      rvest::html_text2() %>%
+      paste(collapse = "\n")
+
+    tibble::tibble(
+      datetime,
+      author,
+      headline,
+      text
+    )
+  }) %>%
+    cbind(x) %>%
+    normalise_df() %>%
+    return()
+}
