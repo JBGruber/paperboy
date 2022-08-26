c9bd4276ddd6953e8652e8189900a39cd0506ae2
JBGruber <gruber@europa-uni.de>
2022-08-26 15:52:24 +0200

    improved template

--- inst/templates/deliver_.R
+++ inst/templates/deliver_.R
@@ -8,58 +8,56 @@ pb_deliver_paper.{{newspaper}} <- function(x, verbose = NULL, ...) {
   # If verbose is not explicitly defined, use package default stored in options.
   if (is.null(verbose)) verbose <- getOption("paperboy_verbose")
 
-  # A small test to check if x is really a tibble.
-  if (!"tbl_df" %in% class(x))
-    stop("Wrong object passed to internal deliver function: ", class(x))
+  class_test(x)
 
   if (verbose) message("\t...", nrow(x), " articles from ", x$domain[1])
 
   # helper function to make progress bar
   pb <- make_pb(x)
 
-  # define parsing function iterate over the URLs
-  parse_{{newspaper}} <- function(i) {
-
-    # raw html is stored in column content_raw
-    html <- rvest::read_html(x$content_raw[i])
-    if (verbose) pb$tick()
-
-    # datetime
-    datetime <- html |>
-      rvest::html_elements("") |>
-      rvest::html_attr("") |>
-      lubridate::as_datetime()
-
-    # headline
-    headline <- html |>
-      rvest::html_elements("") |>
-      rvest::html_attr("")
-
-    # author
-    author <- html |>
-      rvest::html_elements("")  |>
-      rvest::html_text2() |>
-      toString()
-
-    # text
-    text <- html |>
-      rvest::html_elements("") |>
-      rvest::html_text2()
-
-    # the helper function safely makes creates a named list from objects
-    s_n_list(
-      datetime,
-      author,
-      headline,
-      text
-    )
-
-  }
-
   # iterate over all URLs and normalise data.frame
-  purrr::map_df(seq_along(x$url), parse_) |>
+  purrr::map_df(x$content_raw, parse_{{newspaper}}) |>
     cbind(x) |>
     normalise_df() |>
     return()
+
+}
+
+# define parsing function to iterate over the URLs
+parse_{{newspaper}} <- function(html) {
+
+  # raw html is stored in column content_raw
+  html <- rvest::read_html(html)
+  if (verbose) pb$tick()
+
+  # datetime
+  datetime <- html |>
+    rvest::html_elements("") |>
+    rvest::html_attr("") |>
+    lubridate::as_datetime()
+
+  # headline
+  headline <- html |>
+    rvest::html_elements("") |>
+    rvest::html_attr("")
+
+  # author
+  author <- html |>
+    rvest::html_elements("")  |>
+    rvest::html_text2() |>
+    toString()
+
+  # text
+  text <- html |>
+    rvest::html_elements("") |>
+    rvest::html_text2()
+
+  # the helper function safely creates a named list from objects
+  s_n_list(
+    datetime,
+    author,
+    headline,
+    text
+  )
 
 }
