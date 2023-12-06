e11b410b56a44d1d59064bd19a0c05020ce89789
JBGruber <johannesb.gruber@gmail.com>
2023-12-06 18:59:03 +0100

    fix ceskatelevize.cz

--- R/deliver_ceskatelevize_cz.R
+++ R/deliver_ceskatelevize_cz.R
@@ -4,26 +4,17 @@ pb_deliver_paper.ceskatelevize_cz <- function(x, verbose = NULL, pb, ...) {
   # raw html is stored in column content_raw
   html <- rvest::read_html(x$content_raw)
 
-  # data about the article is nicely stored in a json string
-  data <- html %>%
-    rvest::html_elements("[type=\"application/ld+json\"]") %>%
-    rvest::html_text() %>%
-    lapply(jsonlite::fromJSON)
-
-  # usually there are more than one,
-  if (length(data) > 0L) {
-    tp <- purrr::map_chr(data, function(x)
-      purrr::pluck(x, "@type", .default = NA_character_))
-
-    data <- purrr::pluck(data, which(tp == "Article"))
-  }
-
   # datetime
-  datetime <- data$datePublished %>%
+  datetime <- html %>%
+    rvest::html_elements("[type=\"application/json\"]") %>%
+    rvest::html_text() %>%
+    extract("(?<=\"startsAt\":\")\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d{3}") %>%
     lubridate::as_datetime()
 
   # headline
-  headline <- data$headline
+  headline <- html %>%
+    rvest::html_element("[property=\"og:title\"]") %>%
+    rvest::html_attr("content")
 
   # author
   author <- html %>%
@@ -31,20 +22,34 @@ pb_deliver_paper.ceskatelevize_cz <- function(x, verbose = NULL, pb, ...) {
     rvest::html_attr("content") %>%
     toString()
 
+  if (author == "NA") {
+    author <- html %>%
+      rvest::html_element(".article-meta__authors")  %>%
+      rvest::html_text() %>%
+      trimws()
+  }
+
   # text
   text <- html %>%
-    rvest::html_elements(".textcontent p") %>%
+    rvest::html_elements(".article__content p") %>%
     rvest::html_text2() %>%
     paste(collapse = "\n")
 
-  cover_image_url <- data$image
+  cover_image_html <- html %>%
+    rvest::html_element("main img") %>%
+    as.character()
+
+  cover_image_url <- html %>%
+    rvest::html_element("main img") %>%
+    rvest::html_attr("src")
 
   s_n_list(
     datetime,
     author,
     headline,
     text,
-    cover_image_url
+    cover_image_url,
+    cover_image_html
   )
 
 }
