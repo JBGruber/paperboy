778c0b6828fbd14394a2167891569e0cc7cf9c56
JBGruber <JohannesB.Gruber@gmail.com>
2024-05-01 11:14:20 +0200

    adds zeit.de

---
+++ R/deliver_zeit_de.R
@@ -0,0 +1,44 @@
+pb_deliver_paper.zeit_de <- function(x, verbose = NULL, pb, ...) {
+
+  # updates progress bar
+  pb_tick(x, verbose, pb)
+
+  # raw html is stored in column content_raw
+  html <- rvest::read_html(x$content_raw)
+
+  # datetime
+  datetime <- html %>%
+    html_search(selectors = c(
+      ".metadata__date>time",
+      "meta[name=\"date\"]"
+    ), attributes = c(
+      "datetime", "content"
+    )) %>%
+    lubridate::as_datetime()
+
+  # headline
+  headline <- html %>%
+    rvest::html_element("[property=\"og:title\"]") %>%
+    rvest::html_attr("content")
+
+  # author
+  author <- html %>%
+    rvest::html_element("[rel=\"author\"],.metadata__source")  %>%
+    rvest::html_text2() %>%
+    toString()
+
+  # text
+  text <- html %>%
+    rvest::html_elements(".article-body p") %>%
+    rvest::html_text2() %>%
+    paste(collapse = "\n")
+
+  # the helper function safely creates a named list from objects
+  s_n_list(
+    datetime,
+    author,
+    headline,
+    text
+  )
+
+}
