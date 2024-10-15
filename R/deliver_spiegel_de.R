91d61a1090d2e831423b1c68ce9ca451145ddef1
schochastics <david@schochastics.net>
2024-10-15 15:28:23 +0200

    added spiegel

---
+++ R/deliver_spiegel_de.R
@@ -0,0 +1,35 @@
+pb_deliver.spiegel.de <- function(x, verbose = NULL, pb, ...) {
+  pb_tick(x, verbose, pb)
+  # raw html is stored in column content_raw
+  html <- rvest::read_html(x$content_raw)
+
+  datetime <- html %>%
+      html_search(html,c("time"),c("datetime")) %>%
+      lubridate::as_datetime()
+
+  # headline
+  headline <- html %>%
+      rvest::html_elements("title") %>%
+      rvest::html_text()
+
+  # author
+  author <- html %>%
+      rvest::html_nodes("header a.text-black") %>%
+      rvest::html_text2() %>%
+      toString()
+
+  # text
+  text <- html %>%
+      rvest::html_elements("div[data-area = \"body\"]") %>%
+      rvest::html_text2() %>%
+      paste(collapse = "\n")
+
+  # the helper function safely creates a named list from objects
+  s_n_list(
+      datetime,
+      author,
+      headline,
+      text
+  )
+
+}
