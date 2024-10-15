aa8e8ecdf68b79068514108d2433ee14a4698637
schochastics <david@schochastics.net>
2024-10-15 20:50:45 +0200

    added welt.de

---
+++ R/deliver_welt_de.R
@@ -0,0 +1,22 @@
+#' @export
+pb_deliver_paper.welt_de <- function(x, verbose = NULL, pb, ...) {
+    pb_tick(x, verbose, pb)
+    # raw html is stored in column content_raw
+    html <- rvest::read_html(x$content_raw)
+
+    json_txt <- rvest::html_nodes(html, "script[type = \"application/ld+json\"] ")[1] |> rvest::html_text()
+    json_df <- jsonlite::fromJSON(json_txt)
+
+    datetime <- lubridate::as_datetime(json_df$datePublished)
+    headline <- json_df$headline
+    author <- toString(json_df$author$name)
+    text <- trimws(gsub("<[^>]+>", "", json_df$articleBody))
+
+    s_n_list(
+        datetime,
+        author,
+        headline,
+        text,
+        json_df # dumping the whole json data of an article
+    )
+}
