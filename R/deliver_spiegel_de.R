a42d16d0f086d11fcf319f590f648bb33ce32357
JBGruber <johannesb.gruber@gmail.com>
2024-10-15 19:27:53 +0200

    change spiegel function

--- R/deliver_spiegel_de.R
+++ R/deliver_spiegel_de.R
@@ -1,5 +1,5 @@
 #' @export
-pb_deliver_paper.spiegel.de <- function(x, verbose = NULL, pb, ...) {
+pb_deliver_paper.spiegel_de <- function(x, verbose = NULL, pb, ...) {
   pb_tick(x, verbose, pb)
   # raw html is stored in column content_raw
   html <- rvest::read_html(x$content_raw)
@@ -10,17 +10,13 @@ pb_deliver_paper.spiegel.de <- function(x, verbose = NULL, pb, ...) {
 
   # headline
   headline <- html %>%
-      rvest::html_nodes(".font-brandUI .align-middle") %>%
-      rvest::html_text()
-
-  if(length(headline) > 1) {
-    headline <- headline[!grepl("\\n", headline)]
-  }
+      rvest::html_element("article") %>%
+      rvest::html_attr("aria-label")
 
   # author
   author <- html %>%
-      rvest::html_nodes("header a.text-black") %>%
-      rvest::html_text2() %>%
+      rvest::html_element("meta[name=\"author\"]") %>%
+      rvest::html_attr("content") %>%
       toString()
 
   # text
