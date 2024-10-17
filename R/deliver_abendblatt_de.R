9b6eaa905845a0615fc0e12a9d7813e766c48bce
schochastics <david@schochastics.net>
2024-10-17 15:58:45 +0200

    added abendblatt.de

---
+++ R/deliver_abendblatt_de.R
@@ -0,0 +1,27 @@
+#' @export
+pb_deliver_paper.abendblatt_de <- function(x, verbose = NULL, pb, ...) {
+    pb_tick(x, verbose, pb)
+    # raw html is stored in column content_raw
+    html <- rvest::read_html(x$content_raw)
+
+    json_txt <- rvest::html_nodes(html, "script[type = \"application/ld+json\"] ")[2] %>% rvest::html_text()
+    json_df <- jsonlite::fromJSON(json_txt)
+
+    datetime <- lubridate::as_datetime(json_df$datePublished)
+    headline <- json_df$headline
+    author <- toString(json_df$author$name)
+    text <- html %>%
+        rvest::html_nodes(".article-body h3, .article-body p") %>%
+        rvest::html_text2() %>%
+        paste(collapse = "\n")
+
+    s_n_list(
+        datetime,
+        author,
+        headline,
+        text,
+        json_df # dumping the whole json data of an article
+    )
+}
+# rss feed includes pages that cannot be parsed because they are subpages
+# rss feed also includes podcast, which cannot be parsed
