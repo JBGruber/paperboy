f2480b2bfb4ac800022dc945a64c368d30c6856c
schochastics <david@schochastics.net>
2024-10-18 19:18:02 +0200

    added epochtimes.de

---
+++ R/deliver_epochtimes_de.R
@@ -0,0 +1,22 @@
+#' @export
+pb_deliver_paper.epochtimes_de <- function(x, verbose = NULL, pb, ...) {
+    pb_tick(x, verbose, pb)
+    # raw html is stored in column content_raw
+    html <- rvest::read_html(x$content_raw)
+
+    json_txt <- rvest::html_nodes(html, "script[type = \"application/ld+json\"] ")[2] %>% rvest::html_text()
+    json_df <- jsonlite::fromJSON(json_txt)
+
+    datetime <- lubridate::as_datetime(json_df$datePublished)
+    headline <- json_df$headline
+    author <- toString(json_df$author$name)
+    text <- json_df$articleBody
+
+    s_n_list(
+        datetime,
+        author,
+        headline,
+        text,
+        json_df # dumping the whole json data of an article
+    )
+}
