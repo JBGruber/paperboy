20ed71a7f46204d80389b0ede4f1311b2810c978
schochastics <david@schochastics.net>
2024-10-18 19:43:03 +0200

    added jungefreiheit.de

---
+++ R/deliver_jungefreiheit_de.R
@@ -0,0 +1,30 @@
+#' @export
+pb_deliver_paper.jungefreiheit_de <- function(x, verbose = NULL, pb, ...) {
+    pb_tick(x, verbose, pb)
+    # raw html is stored in column content_raw
+    html <- rvest::read_html(x$content_raw)
+
+    json_txt <- rvest::html_nodes(html, "script[type = \"application/ld+json\"] ")[1] %>% rvest::html_text()
+    json_df <- jsonlite::fromJSON(json_txt)
+    json_df <- json_df$`@graph`
+    if (any(json_df$`@type` == "Person")) {
+        author <- toString(json_df$name[json_df$`@type` == "Person"])
+    } else {
+        author <- ""
+    }
+    json_df <- json_df[grepl("NewsArticle", json_df$`@type`), ]
+    datetime <- lubridate::as_datetime(json_df$datePublished)
+    headline <- json_df$headline
+    text <- html %>%
+        rvest::html_nodes(".elementor-widget-container p, .elementor-widget-container h3") %>%
+        rvest::html_text2() %>%
+        paste(collapse = "\n")
+
+    s_n_list(
+        datetime,
+        author,
+        headline,
+        text,
+        json_df # dumping the whole json data of an article
+    )
+}
