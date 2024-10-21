7cd635c60c588ce8182891bd47bd98a2ead58890
schochastics <david@schochastics.net>
2024-10-21 11:19:17 +0200

    added dnn.de

---
+++ R/deliver_dnn_de.R
@@ -0,0 +1,28 @@
+#' @export
+pb_deliver_paper.dnn_de <- function(x, verbose = NULL, pb, ...) {
+    pb_tick(x, verbose, pb)
+    # raw html is stored in column content_raw
+    html <- rvest::read_html(x$content_raw)
+
+    json_txt <- rvest::html_elements(html, "script[type = \"application/ld+json\"] ") %>% rvest::html_text2()
+    if (isTRUE(is.na(json_txt)) || length(json_txt) <= 2) {
+        return(s_n_list())
+    } else {
+        json_df <- jsonlite::fromJSON(json_txt[3])
+
+        datetime <- lubridate::as_datetime(json_df$datePublished)
+        headline <- json_df$headline
+        author <- toString(json_df$author$name)
+        text <- html %>%
+            rvest::html_nodes(".Articlestyled__ArticleBodyWrapper-sc-7y75gq-2 p,.Articlestyled__ArticleBodyWrapper-sc-7y75gq-2 h2") %>%
+            rvest::html_text2() %>%
+            paste(collapse = "\n")
+
+        s_n_list(
+            datetime,
+            author,
+            headline,
+            text
+        )
+    }
+}
