53e3f737c5e1873770c8bd76910ec1945c7db99c
JBGruber <JohannesB.Gruber@gmail.com>
2026-06-03 11:17:03 +0200

    fix wiwo.de

--- R/deliver_wiwo_de.R
+++ R/deliver_wiwo_de.R
@@ -4,18 +4,26 @@ pb_deliver_paper.wiwo_de <- function(x, verbose = NULL, pb, ...) {
     # raw html is stored in column content_raw
     html <- rvest::read_html(x$content_raw)
 
-    json_txt <- rvest::html_elements(html, "script[type = \"application/ld+json\"] ") %>% rvest::html_text2()
+    json_txt <- rvest::html_elements(
+        html,
+        "script[type = \"application/ld+json\"] "
+    ) %>%
+        rvest::html_text2()
     if (isTRUE(is.na(json_txt)) || length(json_txt) == 0) {
         return(s_n_list())
     } else {
-        if (length(json_txt) != 0) { # otherwise the article is paywalled and not scrapeable
+        if (length(json_txt) != 0) {
+            # otherwise the article is paywalled and not scrapeable
             json_df <- jsonlite::fromJSON(json_txt[1])
+            json_df <- json_df[!is.na(json_df$url), ]
 
             datetime <- lubridate::as_datetime(json_df$datePublished)
             headline <- json_df$headline
-            author <- toString(json_df$creator)
+            author <- toString(unlist(json_df$creator))
             text <- html %>%
-                rvest::html_elements(".c-leadtext,.u-richtext h3,.u-richtext p") %>%
+                rvest::html_elements(
+                    ".c-leadtext,.u-richtext h3,.u-richtext p,app-rich-text p"
+                ) %>%
                 rvest::html_text2() %>%
                 .[!grepl("Lesen Sie auch", .)] %>% # Remove links in between
                 paste(collapse = "\n")
