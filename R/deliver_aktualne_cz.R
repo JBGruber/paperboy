077280457344b2e818a9311a1d0d2f25cc80e044
Chen <Sina.Chen@gesis.org>
2026-06-09 08:52:45 +0200

    fix: missing closing brace in deliver_aktualne_cz.R

--- R/deliver_aktualne_cz.R
+++ R/deliver_aktualne_cz.R
@@ -5,48 +5,55 @@ pb_deliver_paper.aktualne_cz <- function(x, verbose = NULL, pb, ...) {
   # raw html is stored in column content_raw
   html <- rvest::read_html(x$content_raw)
 
+  article <- html %>% 
+    rvest::html_elements("script[type='application/ld+json']") %>% 
+    rvest::html_text() %>% 
+    purrr::keep(~ grepl('"NewsArticle"', .x)) %>% 
+    purrr::pluck(1) %>% 
+    jsonlite::fromJSON() %>% 
+    purrr::pluck("@graph") %>% 
+    {\(x) x[x$@type == "NewsArticle", ]}()
+  
   # datetime
-  datetime <- html %>%
-    rvest::html_element("[property=\"article:published_time\"]") %>%
-    rvest::html_attr("content") %>%
+  datetime <- article$datePublished %>%
     lubridate::as_datetime()
-
+ 
   # headline
-  headline <- html %>%
-    rvest::html_element(".article-title") %>%
-    rvest::html_text2()
-
+  headline <- article$headline
+  
   # author
-  author <- html %>%
-    rvest::html_elements(".author__name")  %>%
-    rvest::html_text2() %>%
-    toString()
-
+  author_json <- article$author[[1]]$name |> paste(collapse = ", ")
+  author <-  if (is.null(author_json) || author_json == "") {
+    html |>
+      rvest::html_element("span.e-ui-authors-and-date__author-label") |>
+      rvest::html_text2()
+  } else {
+    author_json
+  }
+  
   # text
   text <- html %>%
-    rvest::html_elements(".article .article__perex,#article-content p") %>%
+    rvest::html_element("div[class*='f-tiptap-content__root']") %>%
+    rvest::html_elements("p, h2") %>%
     rvest::html_text2() %>%
     paste(collapse = "\n")
-
+  
+  # cover image html
   cover_image_html <- html %>%
-    rvest::html_element(".article__photo--opener img") %>%
+    rvest::html_element("img.e-ui-image__img") %>%
     as.character()
-
-  cover_image_url <- html %>%
-    rvest::html_element(".article__photo--opener img") %>%
-    rvest::html_attr("src")
-
-  if (!is.na(cover_image_url)) {
-    cover_image_url <- paste0("https:", cover_image_url)
-  }
-
+  # cover image url 
+  cover_image_url <- article$image$url
+  
+  # date and time URL was accessed
+  accessed <- Sys.time()
   s_n_list(
     datetime,
     author,
     headline,
     text,
+    accessed,
     cover_image_url,
     cover_image_html
   )
-
 }
