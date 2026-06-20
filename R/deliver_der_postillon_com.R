2c96412d3775ffdcbffd9cd2ecc048a0cff63aec
JBGruber <JohannesB.Gruber@gmail.com>
2026-06-20 17:33:51 +0200

    revert to old pipe for backwards compatibility

--- R/deliver_der_postillon_com.R
+++ R/deliver_der_postillon_com.R
@@ -1,54 +1,56 @@
 #' @export
 pb_deliver_paper.der_postillon_com <- function(x, verbose = NULL, pb, ...) {
-    pb_tick(x, verbose, pb)
-  
+  pb_tick(x, verbose, pb)
+
   # get final url
-  final_url <- x$content_raw %>% 
-    rvest::read_html() %>% 
-    rvest::html_element("body a") %>% 
+  final_url <- x$content_raw %>%
+    rvest::read_html() %>%
+    rvest::html_element("body a") %>%
     rvest::html_attr("href")
-  
+
   html <- rvest::read_html(final_url)
 
   # author
-  author <- html |>
-    rvest::html_element("meta[property='article:author']") |>
+  author <- html %>%
+    rvest::html_element("meta[property='article:author']") %>%
     rvest::html_attr("content")
-  
+
   # headline
-  headline <- html |>
-    rvest::html_element("meta[property='og:title']") |>
+  headline <- html %>%
+    rvest::html_element("meta[property='og:title']") %>%
     rvest::html_attr("content")
-  
+
   # text
-  text <- html |>
-    rvest::html_element("#post-body") |>
-    rvest::html_elements("p") |>
-    rvest::html_text2() |>
-    stringr::str_trim() |>
+  text <- html %>%
+    rvest::html_element("#post-body") %>%
+    rvest::html_elements("p") %>%
+    rvest::html_text2() %>%
+    stringr::str_trim() %>%
     paste(collapse = "\n")
-  
+
   # date
-  datetime <- html |>
-    rvest::html_element("#Blog1 time") |>
-    rvest::html_attr("datetime") |>
+  datetime <- html %>%
+    rvest::html_element("#Blog1 time") %>%
+    rvest::html_attr("datetime") %>%
     lubridate::as_datetime()
-  
+
   # adjust author if applicable
   if (author == "Der Postillon") {
     author_tmp <- html %>%
-      rvest::html_element("div[id='post-body'] span[style='font-size: x-small;']") %>%
+      rvest::html_element(
+        "div[id='post-body'] span[style='font-size: x-small;']"
+      ) %>%
       rvest::html_text() %>%
-      sub("; Erstver.*$", "", .)  %>% 
+      sub("; Erstver.*$", "", .) %>%
       stringr::str_remove("(?i);\\s*foto.*$")
     if (author_tmp != "") {
       author <- author_tmp
     }
   }
-  
+
   # date and time accessed
   accessed <- Sys.time()
-  
+
   s_n_list(
     datetime,
     author,
@@ -56,6 +58,4 @@ pb_deliver_paper.der_postillon_com <- function(x, verbose = NULL, pb, ...) {
     text,
     accessed
   )
-
-   
 }
