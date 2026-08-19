379c2bb6edd54aca2f10ca7f803e5bea29c3dbfa
Tom Nicholls <github@tomnicholls.me.uk>
2026-08-19 15:59:42 +0100

    Update deliver_uol_com_br.R and add contributor data
    Satisfy `R CMD check` by removing native pipe use and a false-positive global variable use in pb_deliver_paper.cnnbrasil_com_br, and add a contributor data block to DESCRIPTION.

--- R/deliver_cnnbrasil_com_br.R
+++ R/deliver_cnnbrasil_com_br.R
@@ -15,8 +15,8 @@ pb_deliver_paper.cnnbrasil_com_br <- function(x, verbose = NULL, pb, ...) {
   # If there's a straightforward NewsArticle entry this is our best choice.
   # Recently, articles have had their metadata embedded a layer deep in a graph
   # data block instead.
-  json_df <- jsonlite::fromJSON(json_txt[[1]])[["@graph"]] |>
-    dplyr::filter(`@type` == "NewsArticle")
+  json_df <- jsonlite::fromJSON(json_txt[[1]])[["@graph"]]
+  json_df <- json_df[json_df[["@type"]] == "NewsArticle",]
 
   if (is.null(json_df)) return(s_n_list())
 
