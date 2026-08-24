7590a1bce9af9af4bf5a2fcc3f1eed991f36b2a2
Tom Nicholls <github@tomnicholls.me.uk>
2026-08-24 12:34:48 +0100

    Revise pb_supply interface to take a data.frame

--- R/supply.R
+++ R/supply.R
@@ -4,13 +4,19 @@
 #' HTML by constructing a suitable data.frame. It is broadly equivalent to
 #' \link{pb_collect}, but collects no data.
 #'
-#' @param urls Character vector with original URLs.
-#' @param contents_raw Character vector with raw HTML fetched from these URLs.
-#' @param expanded_urls Character vector with final (normalized and/or
+#' @param data A data.frame with input data. At a minimum this must contain
+#'   a column of original urls and one of raw HTML data fetched from these URLs.
+#' @param url The column name containing the original URLs.
+#' @param content_raw The column name containing the raw HTML fetched from
+#'   these URLs.
+#' @param expanded_url The column name containing final (normalized and/or
 #'   redirected) URLs. If \code{NULL}, \link[curl]{curl_parse_url} is used to
-#'   normalize the URLs in \code{urls}.
-#' @param statuses Integer vector with the HTTP response codes. The default
-#'   assumption is 200 ("OK") for for all pages.
+#'   normalize the URLs in \code{data[[urls]]}.
+#' @param domain The column name containing the domain to be used to select a
+#'   parser by \link{pb_deliver}. If \code{NULL}, the correct domains are
+#'   calculated.
+#' @param status The column name containing HTTP response codes. If \code{NULL},
+#'   assume 200 ("OK") for for all pages.
 #' @param collected_at The \code{POSIXct} time when the data were collected.
 #'   Note that paperboy records a single time per data.frame, not per URL.
 #'
@@ -20,32 +26,61 @@
 #' @seealso \link{pb_collect}
 #'
 #' @examples
-#' pb_supply(
-#'   urls = c('bbc.co.uk/news/articles/cz7dlgejqylo',
-#'            'bbc.co.uk/news/articles/c1l1r1zne1ro'),
-#'   contents_raw = c('<!DOCTYPE html><html lang="en-GB" class="no-js"><he...',
-#'                    '<!DOCTYPE html><html lang="en-GB"><head><meta name=...'),
-#'   expanded_urls = c('https://www.bbc.co.uk/news/articles/cz7dlgejqylo',
-#'                     'https://www.bbc.co.uk/news/articles/c1l1r1zne1ro'),
-#'   collected_at = lubridate::ymd_hms('2026-08-20 11:14:05')
+#' df <- dplyr::tibble(
+#'   url = c('bbc.co.uk/news/articles/cz7dlgejqylo',
+#'           'bbc.co.uk/news/articles/c1l1r1zne1ro'),
+#'   html_data = c('<!DOCTYPE html><html lang="en-GB" class="no-js"><he...',
+#'                 '<!DOCTYPE html><html lang="en-GB"><head><meta name=...'),
+#'   expanded_url = c('https://www.bbc.co.uk/news/articles/cz7dlgejqylo',
+#'                    'https://www.bbc.co.uk/news/articles/c1l1r1zne1ro')
+#' )
+#' pb_supply(df,
+#'           content_raw = 'html_data',
+#'           expanded_url = 'expanded_url',
+#'           collected_at = lubridate::ymd_hms('2026-08-20 11:14:05')
 #' )
 #'
 #' @export
-pb_supply <- function(urls,
-                      contents_raw,
-                      expanded_urls = NULL,
-                      statuses = rep(200L, length(urls)),
+pb_supply <- function(data,
+                      url = "url",
+                      content_raw = "content_raw",
+                      expanded_url = NULL,
+                      domain = NULL,
+                      status = NULL,
                       collected_at = lubridate::now()) {
 
-  n <- length(urls)
-  if (is.null(expanded_urls)) {
+  if (is.null(expanded_url)) {
+    expanded_url <- "expanded_url"
     # When curl is used to fetch web pages it does some normalization. We want
-    # the expanded_urls to be valid and as normalized as possible, though we
+    # expanded_url to be valid and as normalized as possible, though we
     # cannot follow through redirects etc. as we would be able to when actually
     # fetching. If the user would prefer to use actually-final URLs then they
-    # can supply their own in expanded_urls.
-    norm_url <- function(u) curl::curl_parse_url(u, default_scheme = TRUE)$url
-    expanded_urls <- sapply(urls, norm_url)
+    # can supply their own column.
+    norm_url <- function(u) curl::curl_parse_url(u, default_scheme=TRUE)[[url]]
+    data[[expanded_url]] <- sapply(data[[url]], norm_url)
+  }
+  if (is.null(domain)) {
+    domain <- "domain"
+    data[[domain]] <- adaR::ada_get_domain(data[[expanded_url]])
+  }
+  if (is.null(status)) {
+    status <- "status"
+    data[[status]] <- 200L
+  }
+
+  if (any(sapply(c(url, content_raw, expanded_url, domain, status, collected_at),
+                 length) != 1)) {
+    cli::cli_abort("All parameters supplied to pb_supply() must be length 1.",
+                  .envir = paperboy.env
+    )
+  }
+
+  if (nrow(data) != length(unique(data[[url]]))) {
+    paperboy.env$len_urls <- nrow(data)
+    paperboy.env$len_unique_urls <- length(unique(data[[url]]))
+    cli::cli_warn("Only {len_urls} of {len_unique_urls} supplied are unique. Processing duplicates anyway.",
+                  .envir = paperboy.env
+    )
   }
 
   # We do some class-checking here to attempt to guarantee that the outbound
@@ -55,40 +90,29 @@ pb_supply <- function(urls,
   # of pb_collect's current behaviour we would be rude to give them something
   # else (and bugs/crashes as a result of time format differences are all too
   # common).
-  if (length(collected_at) != 1 | !(inherits(collected_at, "POSIXct"))) {
-    cli::cli_abort("Only a single POSIXct time is valid for collected_at.",
+  if (!(inherits(collected_at, "POSIXct"))) {
+    cli::cli_abort("Only a POSIXct time is valid for collected_at.",
                    .envir = paperboy.env)
   }
   # If we have integers (or something that coerces cleanly to them) then fine.
-  if (all(statuses == as.integer(statuses))) {
-    statuses <- as.integer(statuses)
-  } else {
-    cli::cli_abort("Only integer values are valid for statuses.",
+  if (all(data[[status]] != as.integer(data[[status]]))) {
+    cli::cli_warn("Only integer values are valid for statuses. Coercing anyway.",
                    .envir = paperboy.env)
   }
-  if (n != length(expanded_urls) |
-      n != length(statuses)) {
-    cli::cli_abort("All supplied data vectors need to be the same length.",
-                   .envir = paperboy.env)
-  }
-  if (n != length(unique(urls))) {
-    paperboy.env$len_urls <- n
-    paperboy.env$len_unique_urls <- length(unique(urls))
-    cli::cli_warn("Only {len_urls} of {len_unique_urls} supplied are unique. Processing duplicates anyway.",
-                  .envir = paperboy.env
-    )
-  }
+  data[[status]] <- as.integer(data[[status]])
 
-  df <- tibble::tibble(url = urls,
-                       expanded_url = expanded_urls,
-                       domain = adaR::ada_get_domain(expanded_urls),
-                       status = statuses,
-                       content_raw = contents_raw
-  )
+  data <- data %>%
+    dplyr::as_tibble() %>%
+    dplyr::rename(url = url,
+                  expanded_url = expanded_url,
+                  domain = domain,
+                  status = status,
+                  content_raw = content_raw) %>%
+    dplyr::select(url, expanded_url, domain, status, content_raw)
 
-  class(df$content_raw) <- "html_content"
-  attr(df, "paperboy_collected_at") <- collected_at
-  attr(df, "paperboy_data_loc") <- "memory"
+  class(data$content_raw) <- "html_content"
+  attr(data, "paperboy_collected_at") <- collected_at
+  attr(data, "paperboy_data_loc") <- "memory"
 
-  df
+  data
 }
